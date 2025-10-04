487f6ab544b87a0ab61c8dbb87e061aa79e48278
jkchavers <jkchavers@aggies.ncat.edu>
2025-10-03 23:09:16 -0400

    Added loop design documentation
    I Updated README.md to include documentation and ChatGPT usage.  and added comments

--- Assignment5.py
+++ Assignment5.py
@@ -5,17 +5,17 @@ starting_num = current_number
 sequence = ""
 step_count = 0
 
-sequence += " " + str(starting_num)
-while current_number > 1:
+sequence += " " + str(starting_num)#Sets the starting number in the sequence as a string value
+while current_number > 1:#stops when current_number is equal to 1
         
-    if current_number % 2 == 0:
-        current_number /= 2
-        sequence += f" {current_number:.0f}"
-    elif current_number % 2 != 0:
-        current_number = current_number * 3 + 1
-        sequence += f" {current_number:.0f}"
+    if current_number % 2 == 0:#if current_number is even 
+        current_number /= 2 #divide current_number by 2
+        sequence += f" {current_number:.0f}"#Appends the new current_number to the sequence string
+    elif current_number % 2 != 0:#if current_number is odd 
+        current_number = current_number * 3 + 1 #Multiply current_number by 3 and add 1
+        sequence += f" {current_number:.0f}"#Appends the new current_number to the sequence string
     
-    step_count += 1
+    step_count += 1#increments steps by 1
 else:
     #print("Enter starting number:", starting_num)
     print(f"Sequence:{sequence}")
@@ -30,13 +30,13 @@ step_count = 0
 prime = True
 
 
-for number in range(2,user_number - 1):
-    if (user_number % number == 0):
+for number in range(2,user_number - 1):#returns all numbers between 2 and the user number minus 1
+    if (user_number % number == 0):#tests user number against a divisor returned from the range funtion in the for loop
         
-        prime = False
+        prime = False #sets prime to False if a remainder is found. Thus proving the number is not prime
 
         
-if prime == True:
+if prime == True: #prime boolean is always set to true unless changed in the for loop as a result of having a divisor.
     print(f"Testing divisors from 2 to {user_number - 1}...")
     print(f"{user_number} is prime!")
 elif prime == False:
@@ -51,17 +51,18 @@ rowString = ""
 
 print("Multiplication Table")
 
-for headerNum in range(0,10):
+for headerNum in range(0,10):#Header row formatting
     print(f"{baseNums[headerNum]:3}",end="")
     
 print()
+
 for row in range(0,10):
 
-    print(f"{baseNums[row]:2}",end="")
+    print(f"{baseNums[row]:2}",end="")#prints the row with the starting value
 
     for column in range(0,10):
 
-        print(f"{baseNums[row] * baseNums[column]:4}",end="")
+        print(f"{baseNums[row] * baseNums[column]:4}",end="")#This calculates and returns a product from left to right. Then return to outer for loop until the last iteration
     
     print()
 
